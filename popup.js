3dc2d63f602eeb0152194416cf700b00a03dc1a8
ORNAGE ABSTRAKT <divjeet.asb@gmail.com>
2021-11-08 09:05:27 +0530

    added event listeners as due to chromes Content Security Policy

--- popup.js
+++ popup.js
@@ -1,17 +1,93 @@
-function addLink(field, otherField) {
-    document.getElementById(field).style.visibility = 'visible';
-    document.getElementById(otherField).style.visibility = 'hidden';
+document.getElementById("add-button1").addEventListener("click", function (){
+    addLink('input-field1')
+});
+
+document.getElementById("add-button2").addEventListener("click", function (){
+    addLink('input-field2')
+});
+
+document.getElementById("add-button3").addEventListener("click", function (){
+    addLink('input-field3')
+});
+
+document.getElementById("add-button4").addEventListener("click", function (){
+    addLink('input-field4')
+});
+
+document.getElementById("add-button5").addEventListener("click", function (){
+    addLink('input-field5')
+});
+
+document.getElementById("add-button6").addEventListener("click", function (){
+    addLink('input-field6')
+});
+
+document.getElementById("add-button7").addEventListener("click", function (){
+    addLink('input-field7')
+});
+
+document.getElementById("add-button8").addEventListener("click", function (){
+    addLink('input-field8')
+});
+
+document.getElementById("add-button9").addEventListener("click", function (){
+    addLink('input-field9')
+});
+
+document.getElementById("add-button10").addEventListener("click", function (){
+    addLink('input-field10')
+});
+
+function addLink(field1) {
+    document.getElementById(field1).style.visibility = 'visible';
 }
 
-let links = ["b"];
-let names = ["Link 1"];
+
+document.getElementById("submit1").addEventListener("click", function(){
+    assignLink('input-field1', 'link1', 'name1', 'input-take1', 'name-take1')
+})
+
+document.getElementById("submit2").addEventListener("click", function(){
+    assignLink('input-field2', 'link2', 'name2', 'input-take2', 'name-take2')
+})
+
+document.getElementById("submit3").addEventListener("click", function(){
+    assignLink('input-field3', 'link3', 'name3', 'input-take3', 'name-take3')
+})
+
+document.getElementById("submit4").addEventListener("click", function(){
+    assignLink('input-field4', 'link4', 'name4', 'input-take4', 'name-take4')
+})
+
+document.getElementById("submit5").addEventListener("click", function(){
+    assignLink('input-field5', 'link5', 'name5', 'input-take5', 'name-take5')
+})
+
+document.getElementById("submit6").addEventListener("click", function(){
+    assignLink('input-field6', 'link6', 'name6', 'input-take6', 'name-take6')
+})
+
+document.getElementById("submit7").addEventListener("click", function(){
+    assignLink('input-field7', 'link7', 'name7', 'input-take7', 'name-take7')
+})
+
+document.getElementById("submit8").addEventListener("click", function(){
+    assignLink('input-field8', 'link8', 'name8', 'input-take8', 'name-take8')
+})
+
+document.getElementById("submit9").addEventListener("click", function(){
+    assignLink('input-field9', 'link9', 'name9', 'input-take9', 'name-take9')
+})
+
+document.getElementById("submit10").addEventListener("click", function(){
+    assignLink('input-field10', 'link10', 'name10', 'input-take10', 'name-take10')
+})
 
 function assignLink(field, linkVar, nameVar, i, n) {
     document.getElementById(field).style.visibility = 'hidden';
     localStorage.setItem(linkVar , document.getElementById(i).value);
     localStorage.setItem(nameVar, document.getElementById(n).value);
 }
-
 
 
 document.getElementById('button1').setAttribute("href", localStorage.getItem("link1"));
@@ -46,17 +122,47 @@ document.getElementById('btn10').innerText = localStorage.getItem("name10");
 
 
 
+document.getElementById("del1").addEventListener("click", function () {
+    removeLink('link1', 'name1')
+})
+
+document.getElementById("del12").addEventListener("click", function () {
+    removeLink('link2', 'name2')
+})
+
+document.getElementById("del3").addEventListener("click", function () {
+    removeLink('link3', 'name3')
+})
+
+document.getElementById("del4").addEventListener("click", function () {
+    removeLink('link4', 'name4')
+})
+
+document.getElementById("del5").addEventListener("click", function () {
+    removeLink('link5', 'name5')
+})
+
+document.getElementById("del6").addEventListener("click", function () {
+    removeLink('link6', 'name6')
+})
+
+document.getElementById("del7").addEventListener("click", function () {
+    removeLink('link7', 'name7')
+})
+
+document.getElementById("del8").addEventListener("click", function () {
+    removeLink('link8', 'name8')
+})
+
+document.getElementById("del9").addEventListener("click", function () {
+    removeLink('link9', 'name9')
+})
+
+document.getElementById("del10").addEventListener("click", function () {
+    removeLink('link10', 'name10')
+})
+
 function removeLink(linkNum, nameNum){
     localStorage.removeItem(linkNum);
     localStorage.removeItem(nameNum);
 }
-
-
-
-/*
-window.onload = function () {
-    document.getElementById("button").onclick = <function>;
-}
-*/
-
-
